6ad69e2efb092ef147b9da924a129b906b5136da
mihailkl <michaelsushong@gmail.com>
2025-02-15 18:15:34 +0300

    Add files via upload

---
+++ Prac1/app/src/main/java/com/example/kleymenovmd_pr1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kleymenovmd_pr1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
